379667e9e4de420c86cec2827edf263fbaed848b
Can Avcı <can.avci1@kocsistem.com.tr>
2022-01-27 16:31:50 +0300

    sc-borneo was upgraded

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ var { buildSchema } = require("graphql");
 var fs = require("fs");
 const database = require("./configs/database");
 const { PORT } = require("./configs/common");
+
+borneoServer.expressApp.use(borneo.express.json());
+
 borneo.addConnection(
   database.borneoMysqlDb.key,
   database.borneoMysqlDb.connection
 );
 
 borneoServer.expressApp.use((req, res, next) => {
+  if (req.body?.variables?.Token) {
+    const token = req.body.variables.Token;
+    console.log("token is : " + token);
+  }
   req.user = Date.now().toString();
   next();
 });
